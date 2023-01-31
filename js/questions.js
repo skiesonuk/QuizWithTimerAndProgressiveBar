4a474f111965787537f7f4515088fc2c064dd8d2
Sonu Kumar <101453725+skiesonuk@users.noreply.github.com>
2023-02-01 00:09:40 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Jimmy's father has three sons- Paul I and Paul II. Can you guess the name of the third son? ",
+    answer: "Jimmy",
+    options: [
+      "Jimmy",
+      "None",
+      "Jerin",
+      "Paul III"
+    ]
+  },
+    {
+    numb: 2,
+    question: "You're 4th place right now in a race. What place will you be in when you pass the person in 3rd place?",
+    answer: "3rd",
+    options: [
+      "1st",
+      "3rd",
+      "2nd",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many months have 28 days?",
+    answer: "All of them",
+    options: [
+      "1",
+      "All of them",
+      "2",
+      "Depends if there's a leap year or not"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many 0.5cm slices can you cut from a bread that is 25cm long? ",
+    answer: "50",
+    options: [
+      "50",
+      "39",
+      "25",
+      "None of the above."
+    ]
+  },
+    {
+    numb: 5,
+    question: "There are two clocks of different colors: The green clock is broken and doesn't run at all, but the yellow clock loses one second every 24 hours. Which clock is more accurate? ",
+    answer: "The green clock",
+    options: [
+      "The yellow clock",
+      "The green clock",
+      "Both",
+      "Neither"
+    ]
+  },
+];
